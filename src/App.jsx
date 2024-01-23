bc2b75d883084f6448df35b984ad627c06fe196d
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-01-23 15:32:27 +0000

    added second gig

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   const band_description = "Makers' favourite rock band";
   const event_time = '26/01/2024 @20:00';
   const event_location = 'O2 Arena';
+  const queenImageUrl = "/assets/queen.jpg";
+  const akonImageUrl = "/assets/akon.jpg";
 
   return (
     <>
@@ -18,6 +20,14 @@ function App() {
         band_description={band_description}
         event_time={event_time}
         event_location={event_location}
+        image={queenImageUrl}
+        />
+      < Gig 
+        band_name="Akon"
+        band_description="Singer and rapper"
+        event_time="27/01/2024 @20:00"
+        event_location="O2 Arena"
+        image={akonImageUrl}
       />
     </div>
     </>
--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import './Gig.css';
 
 const Gig = (props) => {
-    const imageUrl = "/assets/queen.jpg";
-
     return (    
         <div id="gig-container">
             <h3 id="band-name">{ props.band_name }</h3>
             <div>
-                <img id="band-image" src={imageUrl} alt="Description of the image" />
+                <img id="band-image" src={props.image} alt="Description of the image" />
             </div>
             <p id="band-description">{ props.band_description }</p>
             <p id="event-time">Event date: { props.event_time }</p>
