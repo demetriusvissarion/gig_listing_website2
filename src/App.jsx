25b3d838f0ebfc8274ef01a3b4bcb144609aa221
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-01-26 13:52:56 +0000

    added loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,30 @@ import Joke from "./components/Joke";
 
 function App() {
   const [gigs, setGigs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const URL = `https://makers-gig-backend.onrender.com/events`;
-    fetch(URL)
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error(`HTTP error! Status: ${res.status}`);
-            }
-            return res.json();
-        })
-        .then((data) => {
-            // console.log("Raw Data:", data); // Log raw data
-            setGigs(data);
-        })
-        .catch((error) => console.error("Error fetching todos:", error));
-}, []);
+    // simulate a delay during development
+    const delay = 0;
+    const fetchData = async () => {
+      try {
+        const res = await fetch(URL);
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        const data = await res.json();
+        // console.log("Raw Data:", data);
+        setGigs(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching todos:", error);
+        setLoading(false);
+      }
+    };
+    const timeoutId = setTimeout(fetchData, delay);
+    return () => clearTimeout(timeoutId);
+  }, []);
 
   const [count, setCount] = useState(0);
 
@@ -50,81 +58,63 @@ function App() {
 
   return (
     <>
-      <Joke />
-      <br></br>
-      <br></br>
-      <h1>Favorited Gigs</h1>
-      {favoritedGigs.map((gig) => (
-        <Gig
-          key={gig.band_name}
-          {...gig}
-          isFavourite={gig.isFavourite}
-          toggleFavourite={() => toggleFavourite(gig.band_name)}
-        />
-      ))}
-      <h1>Other Gigs</h1>
-      {otherGigs.map((gig) => (
-        <Gig
-          key={gig.band_name}
-          {...gig}
-          isFavourite={gig.isFavourite}
-          toggleFavourite={() => toggleFavourite(gig.band_name)}
-        />
-      ))}
-      <div>
-        <h1>Counter</h1>
-        <button onClick={incrementCounter}>Increment the counter</button>
-      </div>
-      <br></br>
-      <br></br>
-      <div>
-        <h1>Click Listener</h1>
-        <ClickListener />
-      </div>
-      <br></br>
-      <br></br>
-      <h2>Input Logger App</h2>
-      <InputComponent />
-      <br></br>
-      <br></br>
-      <Die />
-      <br></br>
-      <br></br>
-      <LoginForm />
-      <br></br>
-      <br></br>
-      <TodoList todos={todos} setTodos={setTodos} todo_title='Walk the dog'/>
-      <br></br>
-      <br></br>
+      {loading ? (
+        <div className="loading-container">
+          <div className="loading-animation"></div>
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <>
+          <br />
+          <br />
+          <h1>Favorited Gigs</h1>
+          {favoritedGigs.map((gig) => (
+            <Gig
+              key={gig.band_name}
+              {...gig}
+              isFavourite={gig.isFavourite}
+              toggleFavourite={() => toggleFavourite(gig.band_name)}
+            />
+          ))}
+          <h1>Other Gigs</h1>
+          {otherGigs.map((gig) => (
+            <Gig
+              key={gig.band_name}
+              {...gig}
+              isFavourite={gig.isFavourite}
+              toggleFavourite={() => toggleFavourite(gig.band_name)}
+            />
+          ))}
+          <div>
+            <h1>Counter</h1>
+            <button onClick={incrementCounter}>Increment the counter</button>
+          </div>
+          <br />
+          <br />
+          <div>
+            <h1>Click Listener</h1>
+            <ClickListener />
+          </div>
+          <br />
+          <br />
+          <h2>Input Logger App</h2>
+          <InputComponent />
+          <br />
+          <br />
+          <Die />
+          <br />
+          <br />
+          <LoginForm />
+          <br />
+          <br />
+          <TodoList todos={todos} setTodos={setTodos} todo_title='Walk the dog' />
+          <br />
+          <br />
+          <Joke />
+        </>
+      )}
     </>
   );
 }
 
 export default App;
-
-// We have built a basic backend API which will send a list of events.
-
-// The API is hosted on render here: https://makers-gig-backend.onrender.com/events
-
-// Use a tool like Postman or CURL to send requests and explore this API.
-// Update your gig application to fetch the list of events from this backend the first time the application is loaded, instead of hard coding them.
-// While the events are loading, your application should somehow communicate to the user that the data is loading, either through a spinner, or the word "Loading" etc. It is up to you how to display it.
-// Note: This app is being hosted using the free version of Render, so may take up to 30 seconds to respond if it hasn't been used for a while.
-
-
-// {
-//   band_name: 'Queen',
-//   band_description: "Makers' favourite rock band",
-//   event_time: '26/01/2024 @20:00',
-//   event_location: 'O2 Arena',
-//   image: '/assets/queen.jpg',
-//   isFavourite: false,
-// },
-// {
-//   band_name: 'Akon',
-//   band_description: 'Singer and rapper',
-//   event_time: '27/01/2024 @20:00',
-//   event_location: 'O2 Arena',
-//   image: '/assets/akon.jpg',
-//   isFavourite: false,
-// },
